aa4c8ae0ee5f949bf86ba357f523b8ccb8024c8a
鲁宁 <mcgrady_525@hotmail.com>
2020-05-21 17:19:31 +0800

    添加DBInit demo。

---
+++ EFCoreDemo.DBInit/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace EFCoreDemo.DBInit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new EFCoreDemoDbContext())
+            {
+                var students = ctx.Student.ToList();
+
+                foreach (var item in students)
+                {
+                    Console.WriteLine(string.Format("Id:{0},first_name:{1},last_name:{2}", item.Id, item.FirstName, item.LastName));
+                }
+            }
+
+            Console.WriteLine("OK!");
+            Console.ReadKey();
+        }
+    }
+}
